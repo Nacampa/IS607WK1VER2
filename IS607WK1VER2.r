7a0a67e752c360698381c1561115ac4410d28268
Nacampa <Nacampa@users.noreply.github.com>
2014-08-31 20:26:19 -0400

    Commit again

---
+++ IS607WK1VER2.r
@@ -0,0 +1,12 @@
+# 1 - R version: 3.0.2 R Studio version: 0.98.501
+# 2 - PostgreSQL version: 9.3
+# 3 - Package DMwR is loaded, data set sales is loaded
+#   The number of observations is displayed 
+library(DMwR)
+data(sales)
+head(sales)
+nrow(sales)
+# Number of observations: 401146
+nrom(na,omit(sales))
+# number of observations omitting na: 387010
+
